a5ed547131ae52aafbc77390877102d1fb247ec1
Leuo <leuonim@gmail.com>
2025-10-17 19:37:15 +0700

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// Base script for Website Sekolah MT
-(function () {
-  const yearEl = document.getElementById('year');
-  if (yearEl) yearEl.textContent = String(new Date().getFullYear());
-  console.log('Website Sekolah MT initialized.');
-})();
